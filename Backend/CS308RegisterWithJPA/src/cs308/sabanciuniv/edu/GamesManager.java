b8081fe25dd7f1749563558465c760e107946e6a
inceogluberkin <inceogluberkin@sabanciuniv.edu>
2020-04-16 15:23:02 +0300

    Games manager column-name fix

--- Backend/CS308RegisterWithJPA/src/cs308/sabanciuniv/edu/GamesManager.java
+++ Backend/CS308RegisterWithJPA/src/cs308/sabanciuniv/edu/GamesManager.java
@@ -18,13 +18,13 @@ public class GamesManager {
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("byName/{n}")
+	@Path("byNamee/{n}")
 	public List<Games> getDevice(@PathParam("n") String query)
 	{
 		List<Games> resultList = new ArrayList<Games>();
 		try 
 		{
-			Connection conn = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/XiiV7ZFMQd", "XiiV7ZFMQd", "XHEMcSeLpd");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/MnojkxD0Cc", "MnojkxD0Cc", "O44cHM61gZ");
 			PreparedStatement ps = conn.prepareStatement("Select * from Games WHERE name LIKE " + "'%"+query+"%'"  );
 			ResultSet rs = ps.executeQuery();
 			
@@ -35,20 +35,20 @@ public class GamesManager {
 			while(rs.next())
 			{
 				Games obj = new Games();
-				obj.setName(rs.getString("Name"));
-				obj.setReleaseDate(rs.getString("Release Date"));
-				obj.setDeveloper(rs.getString("Developer"));
-				obj.setPublisher(rs.getString("Publisher"));
-				obj.setPlatforms(rs.getString("Platforms"));
-				obj.setRequiredAge(rs.getInt("Required Age"));
-				obj.setCategories(rs.getString("Categories"));
-				obj.setGenres(rs.getString("Genres"));
-				obj.setSteampsyTags(rs.getString("Steamspy Tags"));
-				obj.setNumberOfPlayers(rs.getString("Number of Players"));
-				obj.setPrice(rs.getDouble("Price"));
-				obj.setRating(rs.getDouble("Rating"));
-				obj.setImageUrl(rs.getString("Image"));
-				obj.setRequirements(rs.getString("Requirements"));
+				obj.setName(rs.getString("name"));
+				obj.setReleaseDate(rs.getString("release_date"));
+				obj.setDeveloper(rs.getString("developer"));
+				obj.setPublisher(rs.getString("publisher"));
+				obj.setPlatforms(rs.getString("platforms"));
+				obj.setRequiredAge(rs.getInt("required_age"));
+				obj.setCategories(rs.getString("categories"));
+				obj.setGenres(rs.getString("genres"));
+				obj.setSteampsyTags(rs.getString("steamspy_tags"));
+				obj.setNumberOfPlayers(rs.getString("number_of_players"));
+				obj.setPrice(rs.getDouble("price"));
+				obj.setRating(rs.getDouble("rating"));
+//				obj.setImageUrl(rs.getString("Image"));
+//				obj.setRequirements(rs.getString("requirements"));
 			}
 			conn.close();
 			conn = null;
@@ -65,27 +65,27 @@ public class GamesManager {
 	{
 		try 
 		{
-			Connection conn = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/XiiV7ZFMQd", "XiiV7ZFMQd", "XHEMcSeLpd");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/MnojkxD0Cc", "MnojkxD0Cc", "O44cHM61gZ");
 			PreparedStatement ps = conn.prepareStatement("Select * from Games WHERE name = ?");
 			ps.setString(1, name);
 			ResultSet rs = ps.executeQuery();
 			Games obj = new Games();
 			while(rs.next())
 			{
-				obj.setName(rs.getString("Name"));
-				obj.setReleaseDate(rs.getString("Release Date"));
-				obj.setDeveloper(rs.getString("Developer"));
-				obj.setPublisher(rs.getString("Publisher"));
-				obj.setPlatforms(rs.getString("Platforms"));
-				obj.setRequiredAge(rs.getInt("Required Age"));
-				obj.setCategories(rs.getString("Categories"));
-				obj.setGenres(rs.getString("Genres"));
-				obj.setSteampsyTags(rs.getString("Steamspy Tags"));
-				obj.setNumberOfPlayers(rs.getString("Number of Players"));
-				obj.setPrice(rs.getDouble("Price"));
-				obj.setRating(rs.getDouble("Rating"));
-				obj.setImageUrl(rs.getString("Image"));
-				obj.setRequirements(rs.getString("Requirements"));
+				obj.setName(rs.getString("name"));
+				obj.setReleaseDate(rs.getString("release_date"));
+				obj.setDeveloper(rs.getString("developer"));
+				obj.setPublisher(rs.getString("publisher"));
+				obj.setPlatforms(rs.getString("platforms"));
+				obj.setRequiredAge(rs.getInt("required_age"));
+				obj.setCategories(rs.getString("categories"));
+				obj.setGenres(rs.getString("genres"));
+				obj.setSteampsyTags(rs.getString("steamspy_tags"));
+				obj.setNumberOfPlayers(rs.getString("number_of_players"));
+				obj.setPrice(rs.getDouble("price"));
+				obj.setRating(rs.getDouble("rating"));
+//				obj.setImageUrl(rs.getString("Image"));
+//				obj.setRequirements(rs.getString("requirements"));
 			}
 			conn.close();
 			conn = null;
